a97710836928df68076cc9d4c9ff5f336beb3271
Li Zheng <lixiaozheng.good@163.com>
2016-07-01 11:18:13 +0800

    add class DbUtils

---
+++ src/CustomerManage/DbUtils.java
@@ -0,0 +1,66 @@
+package CustomerManage;
+
+
+import java.sql.*;
+
+public class DbUtils {
+        //静态代码块
+    static {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("conncetion start!");
+    }
+    //连接数据库
+    public static Connection getConnection(){
+        Connection connection = null;
+        String url = "jdbc:oracle:" + "thin:@127.0.0.1:1521:orcl";
+        String user = "scott";
+        String password = "orcl";
+        try {
+            connection  = DriverManager.getConnection(url,user,password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return  connection;
+    }
+
+
+    //关闭ResultSet
+    public static void closeResultSet (ResultSet resultSet){
+        if (resultSet != null){
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    //关闭PreparedStatement
+    public  static void closePrepareStatement(PreparedStatement preparedStatement){
+    if (preparedStatement != null){
+        try {
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    }
+
+    //关闭数据库connection
+    public static void closeConnection(Connection connection){
+        if (connection !=null){
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+}
+
