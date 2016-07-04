59b3e8de24e7462f33de4890dda1f5ffb13553f1
Li Zheng <lixiaozheng.good@163.com>
2016-07-04 23:56:52 +0800

    add to_repair  and a class

---
+++ src/to_repair/to_repair_stament.java
@@ -0,0 +1,186 @@
+package to_repair;
+
+public class to_repair_stament {
+	private String to_repair_number;
+	private String customer_number;
+	private String to_repair_product_type;
+	private String to_repair_mac_brand;
+	private String to_repair_mac_type;
+	private String to_repair_serial;
+	private String to_repair_lack_part;
+	private String to_repair_trouble_phen;
+	private String to_repair_trouble_type;
+	private String to_repair_facial_check;
+	private String to_repair_start_cmd;
+	private String to_repair_impo_files;
+	private String to_repair_hdd;
+	private String to_repair_pc;
+	private String to_repair_ac;
+	private String to_repair_battery;
+	private String to_repair_cd_driver;
+	private String to_repair_floppy;
+	private String to_repair_time;
+	private String to_repair_price;
+	private String to_repair_state;
+	public to_repair_stament(String to_repair_number, String customer_number,
+			String to_repair_product_type, String to_repair_mac_brand,
+			String to_repair_mac_type, String to_repair_serial,
+			String to_repair_lack_part, String to_repair_trouble_phen,
+			String to_repair_trouble_type, String to_repair_facial_check,
+			String to_repair_start_cmd, String to_repair_impo_files,
+			String to_repair_hdd, String to_repair_pc, String to_repair_ac,
+			String to_repair_battery, String to_repair_cd_driver,
+			String to_repair_floppy, String to_repair_time,
+			String to_repair_price, String to_repair_state) {
+		super();
+		this.to_repair_number = to_repair_number;
+		this.customer_number = customer_number;
+		this.to_repair_product_type = to_repair_product_type;
+		this.to_repair_mac_brand = to_repair_mac_brand;
+		this.to_repair_mac_type = to_repair_mac_type;
+		this.to_repair_serial = to_repair_serial;
+		this.to_repair_lack_part = to_repair_lack_part;
+		this.to_repair_trouble_phen = to_repair_trouble_phen;
+		this.to_repair_trouble_type = to_repair_trouble_type;
+		this.to_repair_facial_check = to_repair_facial_check;
+		this.to_repair_start_cmd = to_repair_start_cmd;
+		this.to_repair_impo_files = to_repair_impo_files;
+		this.to_repair_hdd = to_repair_hdd;
+		this.to_repair_pc = to_repair_pc;
+		this.to_repair_ac = to_repair_ac;
+		this.to_repair_battery = to_repair_battery;
+		this.to_repair_cd_driver = to_repair_cd_driver;
+		this.to_repair_floppy = to_repair_floppy;
+		this.to_repair_time = to_repair_time;
+		this.to_repair_price = to_repair_price;
+		this.to_repair_state = to_repair_state;
+	}
+	public String getTo_repair_number() {
+		return to_repair_number;
+	}
+	public void setTo_repair_number(String to_repair_number) {
+		this.to_repair_number = to_repair_number;
+	}
+	public String getCustomer_number() {
+		return customer_number;
+	}
+	public void setCustomer_number(String customer_number) {
+		this.customer_number = customer_number;
+	}
+	public String getTo_repair_product_type() {
+		return to_repair_product_type;
+	}
+	public void setTo_repair_product_type(String to_repair_product_type) {
+		this.to_repair_product_type = to_repair_product_type;
+	}
+	public String getTo_repair_mac_brand() {
+		return to_repair_mac_brand;
+	}
+	public void setTo_repair_mac_brand(String to_repair_mac_brand) {
+		this.to_repair_mac_brand = to_repair_mac_brand;
+	}
+	public String getTo_repair_mac_type() {
+		return to_repair_mac_type;
+	}
+	public void setTo_repair_mac_type(String to_repair_mac_type) {
+		this.to_repair_mac_type = to_repair_mac_type;
+	}
+	public String getTo_repair_serial() {
+		return to_repair_serial;
+	}
+	public void setTo_repair_serial(String to_repair_serial) {
+		this.to_repair_serial = to_repair_serial;
+	}
+	public String getTo_repair_lack_part() {
+		return to_repair_lack_part;
+	}
+	public void setTo_repair_lack_part(String to_repair_lack_part) {
+		this.to_repair_lack_part = to_repair_lack_part;
+	}
+	public String getTo_repair_trouble_phen() {
+		return to_repair_trouble_phen;
+	}
+	public void setTo_repair_trouble_phen(String to_repair_trouble_phen) {
+		this.to_repair_trouble_phen = to_repair_trouble_phen;
+	}
+	public String getTo_repair_trouble_type() {
+		return to_repair_trouble_type;
+	}
+	public void setTo_repair_trouble_type(String to_repair_trouble_type) {
+		this.to_repair_trouble_type = to_repair_trouble_type;
+	}
+	public String getTo_repair_facial_check() {
+		return to_repair_facial_check;
+	}
+	public void setTo_repair_facial_check(String to_repair_facial_check) {
+		this.to_repair_facial_check = to_repair_facial_check;
+	}
+	public String getTo_repair_start_cmd() {
+		return to_repair_start_cmd;
+	}
+	public void setTo_repair_start_cmd(String to_repair_start_cmd) {
+		this.to_repair_start_cmd = to_repair_start_cmd;
+	}
+	public String getTo_repair_impo_files() {
+		return to_repair_impo_files;
+	}
+	public void setTo_repair_impo_files(String to_repair_impo_files) {
+		this.to_repair_impo_files = to_repair_impo_files;
+	}
+	public String getTo_repair_hdd() {
+		return to_repair_hdd;
+	}
+	public void setTo_repair_hdd(String to_repair_hdd) {
+		this.to_repair_hdd = to_repair_hdd;
+	}
+	public String getTo_repair_pc() {
+		return to_repair_pc;
+	}
+	public void setTo_repair_pc(String to_repair_pc) {
+		this.to_repair_pc = to_repair_pc;
+	}
+	public String getTo_repair_ac() {
+		return to_repair_ac;
+	}
+	public void setTo_repair_ac(String to_repair_ac) {
+		this.to_repair_ac = to_repair_ac;
+	}
+	public String getTo_repair_battery() {
+		return to_repair_battery;
+	}
+	public void setTo_repair_battery(String to_repair_battery) {
+		this.to_repair_battery = to_repair_battery;
+	}
+	public String getTo_repair_cd_driver() {
+		return to_repair_cd_driver;
+	}
+	public void setTo_repair_cd_driver(String to_repair_cd_driver) {
+		this.to_repair_cd_driver = to_repair_cd_driver;
+	}
+	public String getTo_repair_floppy() {
+		return to_repair_floppy;
+	}
+	public void setTo_repair_floppy(String to_repair_floppy) {
+		this.to_repair_floppy = to_repair_floppy;
+	}
+	public String getTo_repair_time() {
+		return to_repair_time;
+	}
+	public void setTo_repair_time(String to_repair_time) {
+		this.to_repair_time = to_repair_time;
+	}
+	public String getTo_repair_price() {
+		return to_repair_price;
+	}
+	public void setTo_repair_price(String to_repair_price) {
+		this.to_repair_price = to_repair_price;
+	}
+	public String getTo_repair_state() {
+		return to_repair_state;
+	}
+	public void setTo_repair_state(String to_repair_state) {
+		this.to_repair_state = to_repair_state;
+	}
+	
+
+}
