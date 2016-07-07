1593835473ac8855e56012eb753a941860480572
Li Zheng <lixiaozheng.good@163.com>
2016-07-07 11:02:47 +0800

    修改功能完善

--- src/servlet/UpdateServlet.java
+++ src/servlet/UpdateServlet.java
@@ -1,6 +1,9 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import CustomerManage.Customer;
 import CustomerManage.CustomerFunManage;
 
 /**
@@ -16,7 +20,17 @@ import CustomerManage.CustomerFunManage;
  */
 public class UpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private int customer_number;
+	 private int customer_number;
+	 private String customer_id;
+	 private Date customer_send_mac;
+	 private String customer_nature;
+	 private String customer_unit;
+	 private String customer_tel;
+	 private String customer_phone;
+	 private String customer_address;
+	 private String customer_postcode;
+	 private String customer_contacts;
+	 private String customer_email;
 	CustomerFunManage FunMa = CustomerFunManage.getInstance();
 
 	/**
@@ -34,9 +48,41 @@ public class UpdateServlet extends HttpServlet {
 		request.setCharacterEncoding("utf-8");
 		response.setCharacterEncoding("utf-8");
 		customer_number = Integer.parseInt(request.getParameter("customer_number"));
-		FunMa.updateCusByNo(customer_number);
+		
+		customer_id = request.getParameter("customer_id");
+		//处理时间转换
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		java.util.Date newDate = null;
+		try {
+			newDate = sdf.parse(request.getParameter("customer_send_mac"));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 customer_send_mac = new Date(newDate.getTime());
+		
+		customer_nature = request.getParameter("customer_nature");
+		customer_unit = request.getParameter("customer_unit");
+		customer_tel = request.getParameter("customer_tel");
+		customer_phone = request.getParameter("customer_phone");
+		customer_address = request.getParameter("customer_address");
+		customer_postcode = request.getParameter("customer_postcode");
+		customer_contacts = request.getParameter("customer_contacts");
+		customer_email = request.getParameter("customer_email");
+	
+		//构造函数调用
+		Customer customer01 = new Customer( customer_number,  customer_id,  customer_send_mac,
+                  customer_nature,  customer_unit,  customer_tel,
+                  customer_phone,  customer_address, customer_postcode,
+                  customer_contacts,  customer_email);
+//		System.out.println("info:\n" + customer.getCustomer_email() + "info:\n" +customer.getCustomer_unit());
+		CustomerFunManage customTest =null;
+		customTest = CustomerFunManage.getInstance();
+		customTest.updateCusByNo(customer01);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("Customer/Test.jsp");			
 		dispatcher.forward(request, response);
+		
+		
 	}
 
 }
